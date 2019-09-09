974360fa685804670bd2dcf9529e467f4e2f02b9
LuisPChavez <luis.p.chavez.1@gmail.com>
2019-09-08 19:53:20 -0700

    Added google auth

---
+++ keys/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  iosClientId:
+    "25767147132-abig4tk0o5lmqqeh1j317133pajasvuu.apps.googleusercontent.com",
+  androidClientId:
+    "25767147132-cg9oc76ht3on52ht2m3o5s49poun3355.apps.googleusercontent.com"
+};
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,82 +1,92 @@
-import React, {PureComponent} from 'react';
-import {Text, View, SafeAreaView, TextInput, ScrollView} from 'react-native';
+import React, { PureComponent } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+import * as Google from "expo-google-app-auth";
+import { iosClientId, androidClientId } from "../../keys";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class ProfileScreen extends PureComponent {
   static navigationOptions = {
     header: null
   };
+
   constructor(props) {
     super(props);
-    this.state = {signedIn: false};
+    this.state = { signedIn: false };
   }
+
+  signInWithGoogle = async () => {
+    // First- obtain access token from Expo's Google API
+    const config = {
+      iosClientId,
+      androidClientId,
+      scopes: ["profile", "email", "openid"]
+    };
+
+    let email;
+    let id;
+
+    const { type, accessToken, user } = await Google.logInAsync(config);
+    if (user !== undefined) {
+      email = user.email;
+      id = user.id;
+      email = email.split("@");
+      console.log(email[email.length - 1]);
+      console.log(id);
+    }
+  };
+
   render() {
+    const {
+      googleSignInButtonStyle,
+      googleButtonTextStyle,
+      googleButtonContainer
+    } = this.styles;
     if (!this.state.signedIn)
       return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#DEE2E2'}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#DEE2E2" }}>
           <Text
             style={{
               fontSize: 28,
-              fontFamily: 'Avenir Next',
-              fontWeight: '600',
+              fontFamily: "Avenir Next",
+              fontWeight: "600",
               marginLeft: 40,
               marginTop: 20
             }}
           >
             Profile
           </Text>
-          <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <View style={{flexDirection: 'row', margin: 10}}>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontFamily: 'Avenir Next',
-                  fontWeight: '500',
-                  margin: 10
-                }}
-              >
-                Email
-              </Text>
-              <TextInput
-                style={{backgroundColor: 'red', width: 200}}
-              ></TextInput>
-            </View>
-            <View style={{flexDirection: 'row', margin: 10}}>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontFamily: 'Avenir Next',
-                  fontWeight: '500',
-                  margin: 10
-                }}
-              >
-                Password
-              </Text>
-              <TextInput
-                style={{backgroundColor: 'red', width: 200}}
-              ></TextInput>
-            </View>
-            <Text
-              style={{
-                marginTop: 50,
-                fontFamily: 'Avenir Next',
-                fontSize: 20,
-                fontWeight: '500'
-              }}
+          <View
+            style={{ justifyContent: "center", alignItems: "center", flex: 1 }}
+          >
+            <TouchableOpacity
+              onPress={this.signInWithGoogle}
+              style={googleSignInButtonStyle}
             >
-              Sign In
-            </Text>
+              <View style={googleButtonContainer}>
+                <Ionicons name="logo-google" size={32} color="white" />
+                <Text style={googleButtonTextStyle}>
+                  Create Account With Google
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </SafeAreaView>
       );
     else {
       return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#DEE2E2'}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#DEE2E2" }}>
           <View>
             <Text
               style={{
                 fontSize: 28,
-                fontFamily: 'Avenir Next',
-                fontWeight: '600',
+                fontFamily: "Avenir Next",
+                fontWeight: "600",
                 marginLeft: 40,
                 marginTop: 20
               }}
@@ -84,12 +94,12 @@ export default class ProfileScreen extends PureComponent {
               Profile
             </Text>
           </View>
-          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
             <View
               style={{
                 width: 150,
                 height: 150,
-                backgroundColor: 'red',
+                backgroundColor: "red",
                 borderRadius: 100,
                 marginTop: 40
               }}
@@ -97,8 +107,8 @@ export default class ProfileScreen extends PureComponent {
             <Text
               style={{
                 fontSize: 22,
-                fontFamily: 'Avenir Next',
-                fontWeight: '600',
+                fontFamily: "Avenir Next",
+                fontWeight: "600",
                 marginTop: 15
               }}
             >
@@ -108,8 +118,8 @@ export default class ProfileScreen extends PureComponent {
           <Text
             style={{
               fontSize: 28,
-              fontFamily: 'Avenir Next',
-              fontWeight: '600',
+              fontFamily: "Avenir Next",
+              fontWeight: "600",
               marginLeft: 40,
               marginTop: 20
             }}
@@ -121,4 +131,25 @@ export default class ProfileScreen extends PureComponent {
       );
     }
   }
+
+  styles = {
+    googleSignInButtonStyle: {
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      backgroundColor: "#3374FF",
+      borderRadius: 6
+    },
+    googleButtonTextStyle: {
+      fontSize: 18,
+      fontFamily: "Avenir Next",
+      fontWeight: "600",
+      color: "#FFFFFF",
+      marginLeft: 10,
+      marginTop: 3
+    },
+    googleButtonContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between"
+    }
+  };
 }
